2d20bb3f8421cc3aab19d24101b859414777e8c4
sophiaswing <sophiaks2026@student.cis.edu.hk>
2024-02-06 11:22:14 +0800

    hello

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.println("Enter your age:");
+        int age = scanner.nextInt();
+        System.out.println("Enter your name:");
+        String firstName = scanner.next();
+        System.out.println("Enter your favourite food:");
+        String favouriteFood = scanner.next();
+        System.out.println("\n" + "First name: " + firstName +"\n \n" + "Age: " + age+"\n \n" + "Favourite Food: " + favouriteFood);
+
 
 
 
